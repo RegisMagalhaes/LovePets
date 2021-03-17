7d1ce880d7b93a3a05399a8d2df55c98c26d1a74
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-03-17 16:12:02 -0300

    Adiciona a interface de GeneroRepository

--- sprint-2-back-end/tarde/01_filmes/senai_filmes_webApi/senai_filmes_webApi/Domains/FilmeDomain.cs
+++ sprint-2-back-end/tarde/01_filmes/senai_filmes_webApi/senai_filmes_webApi/Domains/FilmeDomain.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 
 namespace senai_filmes_webApi.Domains
 {
+    /// <summary>
+    /// Classe que representa a entidade (tabela) Filmes
+    /// </summary>
     public class FilmeDomain
     {
         public int idFilme { get; set; }
