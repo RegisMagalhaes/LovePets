75fb79ab368693f3fd3400ae6c3505cce830fb13
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-04-12 10:30:25 -0300

    Adiciona scripts do banco de dados

---
+++ sprint-2-back-end/exercicios/01_peoples/manha/bd/scripts/senai_peoples_01_DDL.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE M_Peoples;
+GO
+
+USE M_Peoples;
+GO
+
+CREATE TABLE Funcionarios
+(
+	idFuncionario	INT PRIMARY KEY IDENTITY
+	,nome			VARCHAR(250) NOT NULL
+	,sobrenome		VARCHAR(250) NOT NULL
+);
+GO
