b97beabb2657a8c43890357cd419871e36844458
Saulo <saulo.macedoprojetos@gmail.com>
2021-04-13 00:31:18 -0300

    Adiciona resolucao exercicio Peoples

---
+++ sprint-2-back-end/exercicios/01_peoples/senai_peoples_webApi/senai_peoples_webApi/Domains/FuncionarioDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_peoples_webApi.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade Funcionarios
+    /// </summary>
+    public class FuncionarioDomain
+    {
+        public int IdFuncionario { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
