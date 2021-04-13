2ad94a5d8a681f63695269b73086226902f404c7
Saulo <saulo.macedoprojetos@gmail.com>
2021-04-13 01:35:46 -0300

    Adiciona FuncionarioDomain

--- sprint-2-back-end/exercicios/01_peoples/senai_peoples_webApi/senai_peoples_webApi/Domains/FuncionarioDomain.cs
+++ sprint-2-back-end/exercicios/01_peoples/senai_peoples_webApi/senai_peoples_webApi/Domains/FuncionarioDomain.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,13 @@ namespace senai_peoples_webApi.Domains
     public class FuncionarioDomain
     {
         public int IdFuncionario { get; set; }
+
+        [Required(ErrorMessage = "O nome do funcionário é obrigatório!")]
         public string Nome { get; set; }
         public string Sobrenome { get; set; }
+
+        [Required(ErrorMessage = "Informe a data de nascimento do funcionário!")]
+        [DataType(DataType.Date)]
         public DateTime DataNascimento { get; set; }
     }
 }
