5b7f87bebe431dd4d9799f57364d24a0256fe3f4
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-04-15 00:08:04 -0300

    Adiciona funcionalidade buscar usuario por email e senha

---
+++ sprint-2-back-end/exemplos/tarde/01_sqlclient/01_filmes/senai_filmes_webApi/senai_filmes_webApi/Controllers/UsuariosController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using senai_filmes_webApi.Domains;
+using senai_filmes_webApi.Interfaces;
+using senai_filmes_webApi.Repositories;
+
+/// <summary>
+/// Controller responsável pelos endpoints (URLs) referentes aos usuários
+/// </summary>
+namespace senai_filmes_webApi.Controllers
+{
+    // Define que o tipo de resposta da API será no formato JSON
+    [Produces("application/json")]
+
+    // Define que a rota de uma requisição será no formato dominio/api/nomeController
+    // ex: http://localhost:5000/api/Usuarios
+    [Route("api/[controller]")]
+
+    // Define que é um controlador de API
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto _usuarioRepository que irá receber todos os métodos definidos na interface IUsuarioRepository
+        /// </summary>
+        private IUsuarioRepository _usuarioRepository { get; set; }
+
+        /// <summary>
+        /// Instancia o objeto _usuarioRepository para que haja a referência aos métodos no repositório
+        /// </summary>
+        public UsuariosController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        /// <summary>
+        /// Faz a autenticação do usuário
+        /// </summary>
+        /// <param name="login">objeto com os dados de e-mail e senha</param>
+        /// <returns>Um status code e, em caso de sucesso, os dados do usuário buscado</returns>
+        [HttpPost]
+        public IActionResult Login(UsuarioDomain login)
+        {
+            // Busca o usuário pelo e-mail e senha
+            UsuarioDomain usuarioBuscado = _usuarioRepository.BuscarPorEmailSenha(login.email, login.senha);
+
+            // Caso não encontre nenhum usuário com o e-mail e senha informados
+            if (usuarioBuscado == null)
+            {
+                // retorna NotFound com uma mensagem personalizada
+                return NotFound("E-mail ou senha inválidos!");
+            }
+
+            // Caso encontre, retorna um status code 200 com os dados do usuário buscado
+            return Ok(usuarioBuscado);
+        }
+    }
+}
