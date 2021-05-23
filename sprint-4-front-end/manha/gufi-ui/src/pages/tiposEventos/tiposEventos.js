e7163d3da06836e9abf0f43e2889ce6d704c8e4a
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-05-23 01:52:32 -0300

    Adiciona listagem e cadastro de tipos de eventos

--- sprint-4-front-end/manha/gufi-ui/src/pages/tiposEventos/tiposEventos.js
+++ sprint-4-front-end/manha/gufi-ui/src/pages/tiposEventos/tiposEventos.js
@@ -5,13 +5,76 @@ class TiposEventos extends Component{
         super(props);
         this.state = {
             // nomeEstado : valorInicial
-            listaTiposEventos : [ { tipoEventoId : 1, titulo : 'C#' }, { tipoEventoId : 2, titulo : 'ReactJs' } ],
+            listaTiposEventos : [],
             titulo : ''
         }
     }
 
+    buscarTiposEventos = () => {
+        console.log('agora vamos fazer a chamada para a API')
+
+        // Faz a chamada para a API usando o fetch
+        fetch('http://localhost:5000/api/tiposeventos')
+
+        // Fetch retorna uma Promise que se resolve em uma resposta ( Response )
+        // .then(resposta => console.log(resposta))
+
+        // O método .json() retorna um objeto JavaScript
+        // .then(resposta => console.log(resposta.json()))
+
+        // Em outras palavras, define o tipo do dado do retorno da requisição (JSON)
+        .then(resposta => resposta.json())
+        
+        // e atualiza o state listaTiposEventos com os dados obtidos
+        // .then(data => console.log(data))
+        .then(data => this.setState({ listaTiposEventos : data }))
+
+        // Caso ocorra algum erro, mostra no console do navegador
+        .catch( erro => console.log(erro) )
+    }
+
+    // Atualiza o state titulo com o valor do input
+    atualizaEstadoTitulo = async (evento) => {
+                        //  NomeState     Valor do input
+        await this.setState({ titulo : evento.target.value })
+        console.log(this.state.titulo)
+    };
+
+    // Função responsável por cadastrar um Tipo de Evento
+    cadastrarTipoEvento = (event) => {
+        // Ignora o comportamento padrão do navegador
+        event.preventDefault();
+
+        // Faz a chamada para a API usando Fetch
+        fetch('http://localhost:5000/api/tiposeventos', {
+            // Define o método (verbo) da requisição (POST)
+            method : 'POST',
+
+            // Define o corpo da requisição especificando o tipo (JSON)
+            // em outras palavras, converte o state para uma string JSON
+            body : JSON.stringify( { tituloTipoEvento : this.state.titulo } ),
+
+            // Define o cabeçalho da requisição
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+
+        // Exibe no console do navegador a mensagem "Tipo de Evento cadastrado!"
+        .then(console.log("Tipo de Evento cadastrado!"))
+
+        // Caso ocorra algum erro, 
+        // exibe este erro no console do navegador
+        .catch(error => console.log(error))
+
+        // Então, atualiza a lista de Tipos de Eventos
+        // sem o usuário precisar exexcutar qualquer ação
+        .then(this.buscarTiposEventos)
+    }
+
+    // Chama a função buscarTiposEventos assim que o componente é renderizado
     componentDidMount(){
-        // código
+        this.buscarTiposEventos();
     }
 
     render(){
@@ -24,8 +87,8 @@ class TiposEventos extends Component{
                         <table>
                             <thead>
                                 <tr>
-                                    <th>#</th>
-                                    <th>Título</th>
+                                    <th>#</th>{/* IDs */}
+                                    <th>Título</th>{/* Títulos */}
                                 </tr>
                             </thead>
                                 
@@ -34,15 +97,34 @@ class TiposEventos extends Component{
                                     //          Array
                                     this.state.listaTiposEventos.map( (tipoEvento) => {
                                         return (
-                                            <tr key={tipoEvento.tipoEventoId}>
-                                                <td>{tipoEvento.tipoEventoId}</td>
-                                                <td>{tipoEvento.titulo}</td>
+                                            <tr key={tipoEvento.idTipoEvento}>
+                                                <td>{tipoEvento.idTipoEvento}</td>
+                                                <td>{tipoEvento.tituloTipoEvento}</td>
                                             </tr>
                                         )
                                     } )
                                 }
                             </tbody>
                         </table>
+                    </section>
+
+                    <section>
+                        {/* Cadastro de Tipos de Eventos */}
+                        <h2>Cadastro de Tipo de Evento</h2>
+                        
+                        {/* Formulário de cadastro de Tipo de Evento */}
+                        <form onSubmit={this.cadastrarTipoEvento}>
+                            <div>
+                                <input
+                                    type="text"
+                                    value={this.state.titulo}
+                                    onChange={this.atualizaEstadoTitulo}
+                                    placeholder="Título do Tipo de Evento"
+                                />
+
+                                <button type="submit">Cadastrar</button>
+                            </div>
+                        </form>
                     </section>
                 </main>
             </div>
--- sprint-4-front-end/tarde/gufi-ui/src/pages/tiposEventos/tiposEventos.js
+++ sprint-4-front-end/tarde/gufi-ui/src/pages/tiposEventos/tiposEventos.js
@@ -5,13 +5,74 @@ class TiposEventos extends Component{
         super(props);
         this.state = {
             // nomeEstado : valorInicial
-            listaTiposEventos : [ { tipoEventoId : 1, titulo : 'C#' }, { tipoEventoId : 2, titulo : 'ReactJs' }, { tipoEventoId : 3, titulo : 'UX / UI' } ],
+            listaTiposEventos : [],
             titulo : ''
         }
     }
 
+    buscarTiposEventos = () => {
+        console.log('agora vamos fazer a chamada para a API')
+
+        // Faz a chamada para a API usando o fetch
+        fetch('http://localhost:5000/api/tiposeventos')
+
+        // Fetch retorna uma Promise que se resolve em uma resposta ( Response )
+        // .then(resposta => console.log(resposta))
+        
+        // O método .json() retorna um objeto JavaScript
+        // Em outras palavras, define o tipo de dado do retorno da requisição (JSON)
+        .then(resposta => resposta.json())
+        // .then(resposta => console.log(resposta.json()))
+
+        // e atualiza o state listaTiposEventos com os dados obtidos
+        .then(dados => this.setState({ listaTiposEventos : dados }))
+        // .then(dados => console.log(dados))
+
+        // Caso ocorra algum erro, mostra no console do navegador
+        .catch(erro => console.log(erro))
+    }
+
+    // Atualiza o state titulo com o valor do input
+    atualizaEstadoTitulo = async (event) => {
+        //         NomeEstado  :     ValorInput
+        await this.setState({ titulo : event.target.value })
+        console.log(this.state.titulo)
+    }
+
+    // Função responsável por cadastrar um tipo de evento
+    cadastrarTipoEvento = (event) => {
+        // Ignora o comportamento padrão do navegador
+        event.preventDefault();
+
+        // Faz a chamada para a API usando fetch
+        fetch('http://localhost:5000/api/tiposeventos', {
+            // Define o método da requisição (POST)
+            method : 'POST',
+
+            // Define o corpo da requisição especificando o tipo (JSON)
+            // Em outras palavras, converte o state para uma string JSON
+            body : JSON.stringify( { tituloTipoEvento : this.state.titulo } ),
+
+            // Define o cabeçalho da requisição
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        })
+
+        // Exibe no console do navegador a mensagem "Tipo de Evento cadastrado!"
+        .then(console.log('Tipo de Evento cadastrado!'))
+
+        // Caso ocorra algum erro, exibe este erro no console do navegador
+        .catch(erro => console.log(erro))
+
+        // Então, atualiza a lista de Tipos de Evento 
+        // sem o usuário precisar executar outra ação
+        .then(this.buscarTiposEventos)
+    }
+
+    // Chama a função buscarTiposEventos() assim que o componente é renderizado
     componentDidMount(){
-        // código
+        this.buscarTiposEventos();
     }
 
     render(){
@@ -24,24 +85,43 @@ class TiposEventos extends Component{
                         <table>
                             <thead>
                                 <tr>
-                                    <th>#</th> {/* IDs */}
-                                    <th>Título</th> {/* Títulos */}
+                                    <th>#</th>{/* IDs */}
+                                    <th>Título</th>{/* Títulos */}
                                 </tr>
                             </thead>
 
                             <tbody>
                                 {
+                                    //          Array
                                     this.state.listaTiposEventos.map( (tipoEvento) => {
                                         return (
-                                            <tr key={tipoEvento.tipoEventoId}>
-                                                <td>{tipoEvento.tipoEventoId}</td>
-                                                <td>{tipoEvento.titulo}</td>
+                                            <tr key={tipoEvento.idTipoEvento}>
+                                                <td>{tipoEvento.idTipoEvento}</td>
+                                                <td>{tipoEvento.tituloTipoEvento}</td>
                                             </tr>
                                         )
                                     } )
                                 }
                             </tbody>
                         </table>
+                    </section>
+
+                    <section>
+                        {/* Cadastro de Tipo de Evento */}
+                        <h2>Cadastro de Tipo de Evento</h2>
+
+                        {/* Formulário de cadastro de Tipo de Evento */}
+                        <form onSubmit={this.cadastrarTipoEvento}>
+                            <div>
+                                <input
+                                    type="text"
+                                    value={this.state.titulo}
+                                    onChange={this.atualizaEstadoTitulo}
+                                    placeholder="Título do Tipo de Evento"
+                                />
+                                <button type="submit">Cadastrar</button>
+                            </div>
+                        </form>
                     </section>
                 </main>
             </div>
