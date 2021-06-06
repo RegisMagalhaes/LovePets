2094edc72cbd57d774440d51989e204c52435bf0
Saulo <saulo.macedoprojetos@gmail.com>
2021-06-06 01:21:20 -0300

    Adiciona estrutura pagina eventos

--- sprint-4-front-end/manha/gufi-ui/src/pages/eventos/eventos.js
+++ sprint-4-front-end/manha/gufi-ui/src/pages/eventos/eventos.js
@@ -5,9 +5,9 @@ export default class Eventos extends Component {
     constructor(props){
         super(props);
         this.state = {
-
+            
         }
-    }
+    };
 
     // Chama as funções assim que a tela é renderizada
     componentDidMount(){
@@ -15,33 +15,35 @@ export default class Eventos extends Component {
     };
 
     render(){
-        <>
-            <main>
-                <section>
-                    {/* Lista de Eventos */}
-                    <h2>Lista de Eventos</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Evento</th>
-                                <th>Data</th>
-                                <th>Acesso Livre</th>
-                                <th>Tipo de Evento</th>
-                            </tr>
-                        </thead>
+        return(
+            <>
+                <main>
+                    <section>
+                        {/* Lista de Eventos */}
+                        <h2>Lista de Eventos</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Evento</th>
+                                    <th>Data</th>
+                                    <th>Acesso Livre</th>
+                                    <th>Tipo de Evento</th>
+                                </tr>
+                            </thead>
 
-                        <tbody>
+                            <tbody>
 
-                        </tbody>
-                    </table>
-                </section>
+                            </tbody>
+                        </table>
+                    </section>
 
-                <section>
-                    {/* Cadastro de Eventos */}
-                    <h2>Cadastro de Eventos</h2>
-                </section>
-            </main>
-        </>
+                    <section>
+                        {/* Cadastro de Eventos */}
+                        <h2>Cadastro de Eventos</h2>
+                    </section>
+                </main>
+            </>
+        );
     };
 };
