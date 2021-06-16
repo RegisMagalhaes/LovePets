89494045fce244fed22b15fde2c7dbaca76dacb4
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-06-16 16:56:09 -0300

    Remove permissao da API momentaneamente

--- sprint-2-back-end/gufi/tarde/backend/senai_gufi_webApi/senai_gufi_webApi/Startup.cs
+++ sprint-2-back-end/gufi/tarde/backend/senai_gufi_webApi/senai_gufi_webApi/Startup.cs
@@ -36,7 +36,7 @@ namespace senai_gufi_webApi
             services.AddCors(options => {
                 options.AddPolicy("CorsPolicy", 
                     builder => {
-                        builder.WithOrigins("http://localhost:3000")
+                        builder.WithOrigins("http://localhost:3000", "http://localhost:19006")
                                                                     .AllowAnyHeader()
                                                                     .AllowAnyMethod();
                     }
