2cbcbb9e09abf175647e0547d4a8cb51c073f7c7
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-06-18 20:27:32 -0300

    Adiciona funcionalidade login

--- sprint-5-mobile/manha/gufi-mobile/App.js
+++ sprint-5-mobile/manha/gufi-mobile/App.js
@@ -1,82 +1,21 @@
-import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import Convites from './src/screens/convites';
-import Eventos from './src/screens/eventos';
-import Perfil from './src/screens/perfil';
+import Login from './src/screens/login';
+import Main from './src/screens/main';
 
-const bottomTab = createBottomTabNavigator();
+const AuthStack = createStackNavigator();
 
-export default class App extends Component {
-
-  render(){
-    return (
-      <View style={styles.main}>
-        <NavigationContainer>
-          <bottomTab.Navigator
-            initialRouteName= 'Eventos'
-            tabBarOptions={{
-              showLabel: false,
-              showIcon: true,
-              activeBackgroundColor: '#B727FF',
-              inactiveBackgroundColor: '#DD99FF',
-              activeTintColor: '#FFF',
-              inactiveTintColor: '#FFF',
-              style: { height : 50 }
-            }}
-            screenOptions={({ route }) => ({
-              tabBarIcon: () => {
-                if (route.name === 'Convites') {
-                  return(
-                    <Image 
-                      source={require('./assets/img/plane.png')}
-                      style={styles.tabBarIcon}
-                    />
-                  )
-                }
-
-                if (route.name === 'Eventos') {
-                  return(
-                    <Image
-                      source={require('./assets/img/calendar.png')}
-                      style={styles.tabBarIcon}
-                    />
-                  )
-                }
-
-                if (route.name === 'Perfil') {
-                  return(
-                    <Image 
-                      source={require('./assets/img/profile.png')}
-                      style={styles.tabBarIcon}
-                    />
-                  )
-                }
-              }
-            }) }
-          >
-            <bottomTab.Screen name="Convites" component={Convites} />
-            <bottomTab.Screen name="Eventos" component={Eventos} />
-            <bottomTab.Screen name="Perfil" component={Perfil} />
-          </bottomTab.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+export default function Stack(){
+  return(
+    <NavigationContainer>
+      <AuthStack.Navigator
+        headerMode = 'none'
+      >
+        <AuthStack.Screen name = 'Login' component={Login} />
+        <AuthStack.Screen name = 'Main' component={Main} />
+      </AuthStack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-
-  main: {
-    flex: 1,
-    backgroundColor: '#F1F1F1'
-  },
-
-  tabBarIcon: {
-    width: 22,
-    height: 22
-  }
-
-});
