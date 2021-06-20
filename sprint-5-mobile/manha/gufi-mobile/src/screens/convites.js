5153136e8b160670f81655da16f4a20559694d17
Saulo <saulo.macedoprojetos@gmail.com>
2021-06-20 18:23:24 -0300

    Adiciona estrutura perfil e convites

--- sprint-5-mobile/manha/gufi-mobile/src/screens/convites.js
+++ sprint-5-mobile/manha/gufi-mobile/src/screens/convites.js
@@ -1,24 +1,152 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends Component {
+import api from '../services/api';
+
+export default class Convites extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listaMeusEventos: []
+    };
+  }
+
+  buscarMeusEventos = async () => {
+    const resposta = await api.get("/eventos");
+    const dadosDaApi = resposta.data;
+    this.setState({ listaMeusEventos : dadosDaApi });
+  };
+
+  componentDidMount() {
+    // realizar a chamada a api
+    this.buscarMeusEventos();
+  }
 
   render(){
     return (
       <View style={styles.main}>
-          <Text>Convites</Text>
+
+        {/* Cabeçalho - Header */}
+        <View style={styles.mainHeader}>
+          <View style={styles.mainHeaderRow}>
+            <Image 
+              source={require("../../assets/img/plane.png")}
+              style={styles.mainHeaderImg}
+            />
+            <Text style={styles.mainHeaderText}>{"Convites".toUpperCase()}</Text>
+          </View>
+          <View style={styles.mainHeaderLine} />
+        </View>
+
+        {/* Corpo - Body - Section */}
+        <View style={styles.mainBody}>
+          <FlatList 
+            contentContainerStyle={styles.mainBodyContent}
+            data={ this.state.listaMeusEventos }
+            keyExtractor={ item => item.nomeEvento }
+            renderItem={this.renderItem}
+          />
+        </View>
+
       </View>
     );
   }
+
+  renderItem = ({ item }) => (
+
+    <View style={styles.flatItemRow}>
+      <View style={styles.flatItemContainer}>
+        <Text style={styles.flatItemTitle}>{item.nomeEvento}</Text>
+        <Text style={styles.flatItemInfo}>{item.descricao}</Text>
+        <Text style={styles.flatItemInfo}>{item.dataEvento}</Text>
+      </View>
+
+      <View style={styles.flatItemImg}>
+        <Image
+          source={require("../../assets/img/view.png")}
+          style={styles.flatItemImgIcon}
+        />
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
 
+  // conteúdo da main
   main: {
     flex: 1,
-    backgroundColor: '#F1F1F1',
+    backgroundColor: '#F1F1F1'
+  },
+  // cabeçalho
+  mainHeader: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
+  mainHeaderRow: {
+    flexDirection: 'row'
+  },
+  // imagem do cabeçalho
+  mainHeaderImg: {
+    width: 22,
+    height: 22,
+    tintColor: '#ccc',
+    marginRight: -5,
+    marginTop: -12
+  },
+  // texto do cabeçalho
+  mainHeaderText: {
+    fontSize: 16,
+    letterSpacing: 5,
+    color: '#999',
+    fontFamily: 'Open Sans'
+  },
+  // linha de separação do cabeçalho
+  mainHeaderLine: {
+    width: 220,
+    paddingTop: 10,
+    borderBottomColor: '#999',
+    borderBottomWidth: 1
+  },
+  
+  // conteúdo do body
+  mainBody: {
+    flex: 4
+  },
+  // conteúdo da lista
+  mainBodyContent: {
+    paddingTop: 30,
+    paddingRight: 50,
+    paddingLeft: 50
+  },
+  // dados do evento de cada item da lista (ou seja, cada linha da lista)
+  flatItemRow: {
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginTop: 40
+  },
+  flatItemContainer: {
+    flex: 1,
+  },
+  flatItemTitle: {
+    fontSize: 16,
+    color: '#333',
+    fontFamily: 'Open Sans Light'
+  },
+  flatItemInfo: {
+    fontSize: 12,
+    color: '#999',
+    lineHeight: 24
+  },
+  flatItemImg: {
+    justifyContent: 'center'
+  },
+  flatItemImgIcon: {
+    width: 26,
+    height: 26,
+    tintColor: '#B727FF'
+  }
 
 });
--- sprint-5-mobile/tarde/gufi-mobile/src/screens/perfil.js
+++ sprint-5-mobile/tarde/gufi-mobile/src/screens/perfil.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-export default class Perfil extends Component{
+export default class Perfil extends Component {
 
   render(){
     return (
       <View style={styles.main}>
-          <Text>Perfil</Text>
+
+        {/* Cabeçalho - Header */}
+        <View style={styles.mainHeader}>
+          <View style={styles.mainHeaderRow}>
+            <Image 
+              source={require("../../assets/img/profile.png")}
+              style={styles.mainHeaderImg}
+            />
+            <Text style={styles.mainHeaderText}>{"Perfil".toUpperCase()}</Text>
+          </View>
+          <View style={styles.mainHeaderLine} />
+        </View>
+
+        {/* Corpo - Body - Section */}
+        <View style={styles.mainBody}>
+          <View style={styles.mainBodyInfo}>
+            {/* <Image 
+              source={imagem vinda da API}
+              style={styles.mainBodyImg}
+            /> */}
+            <View style={styles.mainBodyImg} />
+
+            <Text style={styles.mainBodyText}>Nome</Text>
+            <Text style={styles.mainBodyText}>E-mail</Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.btnLogout}
+            onPress={this.realizarLogout}
+          >
+              <Text style={styles.btnLogoutText}>Logout</Text>
+          </TouchableOpacity>
+        </View>
+        
       </View>
     );
   }
@@ -17,9 +50,82 @@ const styles = StyleSheet.create({
   // conteúdo da main
   main: {
     flex: 1,
-    backgroundColor: '#F1F1F1',
+    backgroundColor: '#F1F1F1'
+  },
+  // cabeçalho
+  mainHeader: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
-  
+  mainHeaderRow: {
+    flexDirection: 'row'
+  },
+  // imagem do cabeçalho
+  mainHeaderImg: {
+    width: 22,
+    height: 22,
+    tintColor: '#ccc',
+    marginRight: -5,
+    marginTop: -12
+  },
+  // texto do cabeçalho
+  mainHeaderText: {
+    fontSize: 16,
+    letterSpacing: 5,
+    color: '#999',
+    fontFamily: 'Open Sans'
+  },
+  // linha de separação do cabeçalho
+  mainHeaderLine: {
+    width: 220,
+    paddingTop: 10,
+    borderBottomColor: '#999',
+    borderBottomWidth: 1
+  },
+
+  // conteúdo do body
+  mainBody: {
+    flex: 4,
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  // informações do usuário
+  mainBodyInfo: {
+    alignItems: 'center'
+  },
+  mainBodyImg: {
+    backgroundColor: '#ccc',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 50
+  },
+  mainBodyText: {
+    color: '#999',
+    fontSize: 16,
+    marginBottom: 20
+  },
+  // botão de logout
+  btnLogout: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 38,
+    width: 240,
+    backgroundColor: "#FFFFFF",
+    borderColor: "#FFFFFF",
+    borderWidth: 1,
+    borderRadius: 4,
+    shadowOffset: { height: 1, width: 1 },
+    marginBottom: 50
+  },
+  // texto do botão
+  btnLogoutText: {
+    fontSize: 12,
+    fontFamily: "Open Sans Light",
+    color: "#B727FF",
+    letterSpacing: 6,
+    textTransform: 'uppercase'
+  }
+
 });
